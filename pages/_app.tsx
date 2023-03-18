08bafd2c5a7f0e35b7f3f62247d8dc41b7457919
carredo <edoardo.carradori@bakerhughes.com>
2023-03-18 11:58:33 +0000

    NextJS setup + Manual Header I

---
+++ pages/_app.tsx
@@ -0,0 +1,11 @@
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
+import { MoralisProvider } from 'react-moralis'
+
+export default function App({ Component, pageProps }: AppProps) {
+    return (
+        <MoralisProvider initializeOnMount={false}>
+            <Component {...pageProps} />
+        </MoralisProvider>
+    )
+}
