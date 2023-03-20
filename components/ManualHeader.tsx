20f22728342036b6e9beacada70c4a3292e31c94
carredo <edoardo.carradori@bakerhughes.com>
2023-03-20 08:45:48 +0000

    useEffect Hook

--- components/ManualHeader.tsx
+++ components/ManualHeader.tsx
@@ -1,9 +1,23 @@
+import { useEffect } from 'react'
 import { useMoralis } from 'react-moralis'
 
 export default function ManualHeader() {
     // * This is equivalent to the raw use of ethers.js library:
     // ! await window.ethereum.request({ method: "eth_requestAccounts" })
-    const { enableWeb3, account } = useMoralis()
+    const { enableWeb3, account, isWeb3Enabled } = useMoralis()
+
+    // ? This is a React core hook
+    // ? it automatically runs on load
+    // ? then, it'll run checking the value
+    useEffect(() => {
+        console.log(isWeb3Enabled)
+    // ? No dependency array: it runs anytime something re-renders
+    // ! CAREFUL with this>!! Because then you can get circular render
+    // * Black dependency array: runs once on load
+    }
+    , [isWeb3Enabled]
+    // , []
+    )
 
     return (
         <div>
