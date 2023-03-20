3f0e99d35328e6ba8cd34ae70f0da43aa1a871cf
carredo <edoardo.carradori@bakerhughes.com>
2023-03-20 08:26:55 +0000

    Manual Header II

--- components/ManualHeader.tsx
+++ components/ManualHeader.tsx
@@ -3,7 +3,23 @@ import { useMoralis } from 'react-moralis'
 export default function ManualHeader() {
     // * This is equivalent to the raw use of ethers.js library:
     // ! await window.ethereum.request({ method: "eth_requestAccounts" })
-    const { enableWeb3 } = useMoralis()
+    const { enableWeb3, account } = useMoralis()
 
-    return <div>Hi from Header!</div>
+    return (
+        <div>
+            {account ? (
+                <div>
+                    Connected to {account.slice(0, 6)}...{account.slice(account.length - 4)}
+                </div>
+            ) : (
+                <button
+                    onClick={async () => {
+                        await enableWeb3()
+                    }}
+                >
+                    Connect
+                </button>
+            )}
+        </div>
+    )
 }
