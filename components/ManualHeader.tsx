c4cf8cfa5455b7c89d302e4b56f6f2e2501b6ea4
carredo <edoardo.carradori@bakerhughes.com>
2023-03-20 08:17:28 +0000

    React Hooks

--- components/ManualHeader.tsx
+++ components/ManualHeader.tsx
@@ -1,6 +1,8 @@
 import { useMoralis } from 'react-moralis'
 
 export default function ManualHeader() {
+    // * This is equivalent to the raw use of ethers.js library:
+    // ! await window.ethereum.request({ method: "eth_requestAccounts" })
     const { enableWeb3 } = useMoralis()
 
     return <div>Hi from Header!</div>
