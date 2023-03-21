3a034899eb4f6d0f4b6bd37961b32623f3ff0a14
carredo <edoardo.carradori@bakerhughes.com>
2023-03-21 16:31:29 +0000

    WIP: Automatic Constant Value UI Updater

--- components/LotteryEntrance.tsx
+++ components/LotteryEntrance.tsx
@@ -3,13 +3,16 @@
 import { useWeb3Contract } from 'react-moralis'
 
 export default function LotteryEntrance() {
-    const { runContractFunction: enterLottery } = useWeb3Contract({
+    
+    
+    
+    /* const { runContractFunction: enterLottery } = useWeb3Contract({
         abi: undefined,
         contractAddress: '',
         functionName: '',
         params: {},
         msgValue: '',
-    })
+    }) */
 
     return <div>Hi from lottery entrance!</div>
 }
