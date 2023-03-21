2cebc732686bf3e502bbd4be97070220ccc66d81
carredo <edoardo.carradori@bakerhughes.com>
2023-03-21 16:05:05 +0000

    Introduction to Calling Functions in NextJS

--- components/LotteryEntrance.tsx
+++ components/LotteryEntrance.tsx
@@ -1,20 +1,15 @@
 // TODO: have a function to enter the lottery
 
-import { useWeb3Contract } from "react-moralis"
+import { useWeb3Contract } from 'react-moralis'
 
 export default function LotteryEntrance() {
-
-    const { runContractFunction } = useWeb3Contract({
+    const { runContractFunction: enterLottery } = useWeb3Contract({
         abi: undefined,
-        contractAddress: "",
-        functionName:"",
-        params:{},
-        msgValue: ""
+        contractAddress: '',
+        functionName: '',
+        params: {},
+        msgValue: '',
     })
 
-    return (
-        <div>
-            Hi from lottery entrance!
-        </div>
-    )
+    return <div>Hi from lottery entrance!</div>
 }
