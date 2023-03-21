22f54ecdd3a81468c070666b215fb545977cb03e
carredo <edoardo.carradori@bakerhughes.com>
2023-03-21 09:32:45 +0000

    fixed dependencies issues!

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,7 @@
 import Head from 'next/head'
-import { Inter } from 'next/font/google'
 // import ManualHeader from '@/components/ManualHeader'
 import Header from '@/components/Header'
 import LotteryEntrance from '@/components/LotteryEntrance'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
     return (
