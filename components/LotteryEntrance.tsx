03ebda59a428c1dcfbffe4821ecb0863449c521a
Edoardo Carradori <edoardo.carradori.96@gmail.com>
2023-03-22 10:56:45 +0000

    useNotification

--- components/LotteryEntrance.tsx
+++ components/LotteryEntrance.tsx
@@ -4,7 +4,8 @@ import { useWeb3Contract } from 'react-moralis'
 import { abi, contractAddresses } from '../constants'
 import { useMoralis } from 'react-moralis'
 import { useEffect, useState } from 'react'
-import { ethers, BigNumber } from 'ethers'
+import { ethers, BigNumber, ContractTransaction } from 'ethers'
+import { useNotification } from 'web3uikit'
 
 interface ContractAddressesInterface {
     [key: string]: string[]
@@ -15,8 +16,11 @@ export default function LotteryEntrance() {
     const addresses: ContractAddressesInterface = contractAddresses
     const chainId: string = parseInt(chainIdHex!).toString()
     const lotteryAddress = chainId in addresses ? addresses[chainId][0] : null
+
     // ? useStateHook: needed to trigger the useEffect rerendering
     const [entranceFee, setEntranceFee] = useState('0')
+
+    const dispatch = useNotification()
 
     const { runContractFunction: enterLottery } = useWeb3Contract({
         abi: abi,
@@ -46,6 +50,21 @@ export default function LotteryEntrance() {
         }
     }, [getEntranceFee, isWeb3Enabled, entranceFee])
 
+    const hanldeSuccess = async (transaction: ContractTransaction) => {
+        await transaction.wait(1)
+        handleNewNotification(transaction)
+    }
+
+    const handleNewNotification = (transaction: ContractTransaction) => {
+        dispatch({
+            type: 'info',
+            message: 'Transaction Complete!',
+            title: 'Transaction Notification',
+            position: 'topR',
+            icon: 'bell',
+        })
+    }
+
     return (
         <div>
             Hi from lottery entrance!
@@ -53,7 +72,11 @@ export default function LotteryEntrance() {
                 <div>
                     <button
                         onClick={async () => {
-                            await enterLottery()
+                            await enterLottery({
+                                onSuccess: (transaction) =>
+                                    hanldeSuccess(transaction as ContractTransaction),
+                                onError: (error: Error) => console.log(error),
+                            })
                         }}
                     >
                         Enter Lottery
@@ -61,7 +84,7 @@ export default function LotteryEntrance() {
                     Entrance Fee: {ethers.utils.formatUnits(entranceFee, 'ether')} ETH
                 </div>
             ) : (
-                <div>No Lottery Address Detected!</div>
+                <div>No Lottery Address Detected...</div>
             )}
         </div>
     )
